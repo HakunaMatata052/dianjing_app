1d7a1ea7db35229a63cea35f97ea84d6f3f62895
chengluyao <838619567@qq.com>
2019-11-26 11:11:09 +0800

    Signed-off-by: chengluyao <838619567@qq.com>

--- src/views/pk/pkDetails.vue
+++ src/views/pk/pkDetails.vue
@@ -5,14 +5,14 @@
             <div class="pk_chunk">
                 <div class="odd">对战游戏类型：<span>王者荣耀</span></div>
                 <div class="even">游戏ID (昵称)：<span>李白</span></div>
-                <div class="odd">挑战金： <span class="number">1000</span></div>
+                <div class="odd">挑战金： <span class="number" v-text="1000+'元'"></span></div>
                 <div class="even">个人对决赛</div>
                 <div class="odd">可接受挑战时间段：<span class="date">15:30-18:30</span></div>
             </div>
 
             <div class="pk_chunk2">
                 <div>对战规则：</div>
-                <div>对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则</div>
+                <div>对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则对战规则</div>
             </div>
 
             <div class="pk_chunk3">
@@ -43,17 +43,20 @@
         line-height: 40px;
         font-size: 15px;
         text-indent: 15.5px;
-        font-weight:500;
-        color:rgba(51,51,51,1);
-        >div{
-            .number{
-                color:rgba(238,0,0,1);
-                font-weight:bold;
+        font-weight: 500;
+        color: rgba(51, 51, 51, 1);
+
+        > div {
+            .number {
+                color: rgba(238, 0, 0, 1);
+                font-weight: bold;
             }
-            .date{
-                color:rgba(153,153,153,1);
+
+            .date {
+                color: rgba(153, 153, 153, 1);
             }
         }
+
         .odd {
             background: white;
         }
@@ -62,12 +65,15 @@
             background: #fafafa;
         }
     }
+
     .pk_chunk2 {
         margin-top: 15.5px;
-        padding: 18.5px 15.5px;
+        padding: 18.5px 15.5px 40px 15.5px;
         background-color: white;
+
         > div:nth-child(1) {
             font-size: 15px;
+            line-height: 30px;
             font-weight: 500;
             color: rgba(51, 51, 51, 1);
         }
@@ -80,9 +86,11 @@
             line-height: 18px;
         }
     }
+
     .pk_chunk3 {
         padding: 60px 0;
         text-align: center;
+
         > div {
             width: 270px;
             height: 44px;
@@ -92,6 +100,7 @@
             font-size: 17px;
             margin: 0 auto;
             font-weight: bold;
+            color: #333;
         }
     }
 </style>
